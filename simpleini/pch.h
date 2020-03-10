ed8699b72ebf1dde442c59b27bf3b27d1d1f8550
Michael Fadely <michaelwfadely@gmail.com>
2020-03-10 13:03:53 -0600

    get rid of project template garbage

--- simpleini/pch.h
+++ simpleini/pch.h
@@ -1,13 +1,5 @@
-// pch.h: This is a precompiled header file.
-// Files listed below are compiled only once, improving build performance for future builds.
-// This also affects IntelliSense performance, including code completion and many code browsing features.
-// However, files listed here are ALL re-compiled if any one of them is updated between builds.
-// Do not add files here that you will be updating frequently as this negates the performance advantage.
+#pragma once
 
-#ifndef PCH_H
-#define PCH_H
-
-// add headers that you want to pre-compile here
 #include <algorithm>
 #include <cctype>
 #include <cstdint>
@@ -17,5 +9,3 @@
 #include <memory>
 #include <string>
 #include <utility>
-
-#endif //PCH_H
